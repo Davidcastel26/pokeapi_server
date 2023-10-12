7d2ea56f549800f7a7839c501be6753fed763183
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-12 16:13:09 -0600

    removing build dir from repo

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./models/server");
-server_1.app.listen();
-//# sourceMappingURL=index.js.map
--- build/routes/type.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const type_controllers_1 = require("../controllers/type.controllers");
-const router = (0, express_1.Router)();
-router.get('/', type_controllers_1.getTypes);
-router.get('/:idTypes', type_controllers_1.getTypesById);
-exports.default = router;
-//# sourceMappingURL=type.routes.js.map
